09b97c19ca2113a8d0675630359c3fe643fc56cd
otavio xavier <otavioxaviermarinho@gmail.com>
2024-07-03 19:48:42 -0300

    :tada:init: initial commit

---
+++ src/components/ItemMenu.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import ArrowDown from "@/assets/arrow-down.svg";
+
+type Props = {
+    name: string,
+   }
+
+export function ItemMenu( {name} : Props ) {
+  return (
+    <div>
+      <button className="flex items-center gap-3">
+        <span className="text-white font-bold">{name}</span>
+        <Image src={ArrowDown} alt="Arrow dropdown" />
+      </button>
+    </div>
+  );
+}
